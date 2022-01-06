21e7b2a6d11b99865758ac21d8f1955439a0749f
Douglas Pinheiro Goulart <douglaspigoulart@gmail.com>
2022-01-06 19:57:18 -0300

    Creating Main component styles

--- src/components/Main/test.tsx
+++ src/components/Main/test.tsx
@@ -12,4 +12,10 @@ describe('<Main />', () => {
 
     expect(container.firstChild).toMatchSnapshot()
   })
+
+  it('should render colors correctly', () => {
+    const { container } = render(<Main />)
+
+    expect(container.firstChild).toHaveStyle({ 'background-color': '#06092b' })
+  })
 })
