4cd975b7c6e0b7b14443e9d31c87e2aaaa587d5d
Douglas Pinheiro Goulart <douglaspigoulart@gmail.com>
2022-01-07 21:19:06 -0300

    Adding storybook

---
+++ src/components/Main/stories.tsx
@@ -0,0 +1,10 @@
+import Main from '.'
+import { withKnobs, text } from '@storybook/addon-knobs'
+
+export default {
+  title: 'Main',
+  component: Main,
+  decorators: [withKnobs]
+}
+
+export const Basic = () => <Main title={text('Title', 'React Avançadoooo')} />
